7100eb584a3b45c1dc05074579bbade5eb12b685
RUBESHR7 <jarvistechnology1.com>
2024-12-24 05:02:23 +0530

    Third task

---
+++ datatypes and logical operators.py
@@ -0,0 +1,43 @@
+String = 'Hello'
+print (type(String))
+
+Integer = 4
+print (type(Integer))
+
+Float = 2.0
+print (type(Float))
+
+List = [1,2,3]
+print (type(List))
+
+Set = {1,2,3}
+print (type(Set))
+
+Tuple = (1,2,3)
+print (type(Tuple))
+
+Boolean = True
+print (type(Boolean))
+
+Range = range(2)
+print (type(Range))
+
+none = None
+print (type(None))
+
+Dictionary = {'name':'Rubesh','age': 0}
+print (type(Dictionary))
+
+Complex = 1 + 4j
+print(type(Complex))
+
+print(2+7>7-2 and 4>6)
+print(2+6<12-3 and 4<6)
+print(10/2<=5 or 10>9)
+print(10/2>8 or 10<9)
+
+x = True
+print(not x)
+
+x = False
+print(not x)
